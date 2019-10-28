896915a264fa36babe4c798320930ab59b5ed352
DariaVasilega <dariavasilega@gmail.com>
2019-10-28 02:41:38 +0200

    Created entity classes for all the entities; made CRUD for all entities

---
+++ src/Entity/Department.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\DepartmentRepository")
+ */
+class Department
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Title;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Description;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Team_lead;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->Title;
+    }
+
+    public function setTitle(string $Title): self
+    {
+        $this->Title = $Title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->Description;
+    }
+
+    public function setDescription(string $Description): self
+    {
+        $this->Description = $Description;
+
+        return $this;
+    }
+
+    public function getTeamLead(): ?string
+    {
+        return $this->Team_lead;
+    }
+
+    public function setTeamLead(string $Team_lead): self
+    {
+        $this->Team_lead = $Team_lead;
+
+        return $this;
+    }
+}
