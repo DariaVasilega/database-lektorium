22d99673b123e2727965f5ff9f10dbb58f311867
DariaVasilega <dariavasilega@gmail.com>
2019-11-03 23:05:23 +0200

    create Home page and made changes in templates

--- src/Controller/ProjectPeopleController.php
+++ src/Controller/ProjectPeopleController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/project/people")
+ * @Route("/people")
  */
 class ProjectPeopleController extends AbstractController
 {
